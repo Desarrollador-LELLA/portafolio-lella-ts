6ea096a38f7b840baecaa407792115e11baf3717
Luis Esteban Llancamil Aguilera <luis.llancamil.a@gmail.com>
2023-03-19 04:48:30 -0300

    Actualizacion libreria firebase

---
+++ src/firebaseInicial/firebase.ts
@@ -0,0 +1,11 @@
+import fireConfig from './config';
+import { initializeApp } from 'firebase/app';
+import * as allAna from "firebase/analytics";
+import * as allAuth from 'firebase/auth';
+
+const app = initializeApp(fireConfig);
+const ana = allAna.getAnalytics(app);
+const auth = allAuth.getAuth(app);
+
+export default app;
+export { auth, allAuth, ana, allAna };
